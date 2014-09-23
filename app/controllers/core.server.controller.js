f6a9e4d33bdcef7b98174bccbff29445c932a395
Guillermo Hernán De Souza <hds@fansworld.tv>
2014-09-23 14:10:03 -0300

    warming up /qr route

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -6,12 +6,14 @@ var qr = require('qr-image');
  * Module dependencies.
  */
 exports.index = function(req, res) {
-	var code = qr.image('http://www.google.com', { type: 'svg' });
-  	//res.type('svg');
-  	//code.pipe(res);
 
 	res.render('index', {
 		user: req.user || null,
-		code: code
 	});
 };
+
+exports.qr = function(req, res) {
+	var code = qr.image('http://www.google.com', { type: 'svg' });
+  	res.type('svg');
+  	code.pipe(res);
+}
--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -4,4 +4,8 @@ module.exports = function(app) {
 	// Root routing
 	var core = require('../../app/controllers/core');
 	app.route('/').get(core.index);
+
+	// QR routing
+	var core = require('../../app/controllers/core');
+	app.route('/qr').get(core.qr);
 };
