8fb1139bd7fb04995c1df5b0f043adcfa9be9bb6
H3rN4n <desouza030689@gmail.com>
2014-10-03 21:11:22 -0300

    working

--- app/controllers/petimages.server.controller.js
+++ app/controllers/petimages.server.controller.js
@@ -6,7 +6,66 @@
 var mongoose = require('mongoose'),
 	errorHandler = require('./errors'),
 	Petimage = mongoose.model('Petimage'),
+	formidable = require('formidable'),
+    util = require('util'),
+    fs   = require('fs-extra'),
+    qt   = require('quickthumb'),
 	_ = require('lodash');
+
+
+// Use quickthumb
+//app.use(qt.static(__dirname + '/'));
+
+/*app.post('/upload', function (req, res){
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files) {
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.write('received upload:\n\n');
+    res.end(util.inspect({fields: fields, files: files}));
+  });
+
+  form.on('end', function(fields, files) {
+    /* Temporary location of our uploaded file 
+    var temp_path = this.openedFiles[0].path;
+    /* The file name of the uploaded file 
+    var file_name = this.openedFiles[0].name;
+    /* Location where we want to copy the uploaded file 
+    var new_location = 'uploads/';
+
+    fs.copy(temp_path, new_location + file_name, function(err) {  
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("success!")
+      }
+    });
+  });
+});*/
+exports.upload = function(req, res) {
+	var form = new formidable.IncomingForm();
+  	form.parse(req, function(err, fields, files) {
+	    res.writeHead(200, {'content-type': 'text/plain'});
+	    res.write('received upload:\n\n');
+	    res.end(util.inspect({fields: fields, files: files}));
+	  });
+
+	  form.on('end', function(fields, files) {
+	    /* Temporary location of our uploaded file */
+	    var temp_path = this.openedFiles[0].path;
+	    /* The file name of the uploaded file */
+	    var file_name = this.openedFiles[0].name;
+	    /* Location where we want to copy the uploaded file */
+	    var new_location = 'uploads/';
+
+	    fs.copy(temp_path, new_location + file_name, function(err) {  
+	      if (err) {
+	        console.error(err);
+	      } else {
+	        console.log('success!');
+	      }
+	    });
+	  });
+};
 
 /**
  * Create a Petimage
